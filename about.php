ade859495471f25350b6ef180661453fbe2ed36d
LCW <laurenconnellwhitney@gmail.com>
2019-06-28 02:20:41 -0400

    Info Update and Styling About pg

--- about.php
+++ about.php
@@ -20,7 +20,9 @@
         <div class="about-right col-sm-6">
             <div class="about-text">
             <h1>Aspasia Academy </h1>
-            <p>is a place for women to share their stories of autonomy, feminism, entrepreneurship, artistic creation and technological learning. There is so much to understand through a direct learned experience, and sometimes its harder than you think to find a mentor or teacher in your reach. Sometimes all that is needed is to hear another's perspective. This platform is a place where women can share ideas, mark a daily practice, and connect with other creators and artists. Most importantly a place to share your creative experience with others from around the world.</p>
+            <p>is a place for women to remember the power of creative practice. To share our stories of autonomy, feminism, entrepreneurship, artistic creation and technological learning.</p>
+            <p>While making and iterating it is easy to get lost in the details, forgetting the pleasure of exploration. Remembering to listen and learn from others is a habit that needs a daily reminder.</p>
+            <p>There is so much to understand through a direct learned experience, and sometimes its harder than you think to find a mentor or teacher in your reach. This is a place where women can share ideas, mark a daily practice, and connect with other creators and artists. Most importantly a place to share your creative experience with others.<p>Exploration as creation.</p>
             </div>
         </div>
     </div>  
--- index.php
+++ index.php
@@ -11,24 +11,26 @@
     <div class="hero-image-1 mt-6">
         <div class="hero-text-1">
             <h1>Creative Practicing</h1>
-            <p>The fruits of a daily practice can be useful in more ways than the final output. Sometimes all we need is a little repetition.</p>
-            <button>Track your progress</button>
+            <p>One doesn’t arrive — in words or in art — by necessarily knowing where one is going. In every work of art something appears that does not previously exist, and so, by default, you work from what you know to what you don’t know.</p>
+            <p>The responsibility of the artist, is the practice of recognizing.</p>
+            <p>A life of making isn’t a series of shows, or projects, or productions, or things: it is an everyday practice. It is a practice of questions more than answers, of waiting to find what you need more often than knowing what you need to do.<br><i>- Ann Hamilton</i></p>
+            <button>Recognize your practice</button>
         </div>
     </div>
 
     <div class="hero-image-2 mt-6">
         <div class="hero-text-2">
-            <h1>Creative Practicing</h1>
-            <p>from aspasia academy</p>
-            <button>add your story</button>
+            <h1>On Creation</h1>
+            <p>The monster I kill every day is the monster of realism. The monster who attacks me every day is destruction. Out of the duel comes the transformation. I turn destruction into creation over and over again. <i>- Anaïs Nin</i></p>
+            <button>Iterate & transform</button>
         </div>
     </div>
 
     <div class="hero-image-3 mt-6">
         <div class="hero-text-1">
-            <h1>Creative Practicing</h1>
-            <p>from aspasia academy</p>
-            <button>add your story</button>
+            <h1>On Reading</h1>
+            <p>Nearly every book has the same architecture — cover, spine, pages — but you open them onto worlds and gifts far beyond what paper and ink are, and on the inside they are every shape and power. Some books are toolkits you take up to fix things, from the most practical to the most mysterious, from your house to your heart, or to make things, from cakes to ships...They can be doorways and ships and fortresses for anyone who loves them. <i>- Rebecca Solnit</i></p>
+            <button>Share your story</button>
         </div>
     </div>
 
